9ce13d50ed019d0f0aaca190691776bc865a6874
ofekz1997 <63488509+ofekz1997@users.noreply.github.com>
2022-05-21 13:44:42 +0300

    update OptimumSet, calcAllSons to use node_cache.

--- fleet_offers.py
+++ fleet_offers.py
@@ -229,6 +229,7 @@ def get_fleet_offers(
 			best=None
 
 		# Brute-Force Algorithm- optimal results / more complex
+		# start_time = time.time()
 		# groups = create_groups(
 		# 	updated_params, app_size
 		# )  ## creates all the possible combinations
@@ -240,10 +241,11 @@ def get_fleet_offers(
 		# 	res += calculator.get_offers(
 		# 		combination, region_to_check, pricing, architecture, type_major
 		# 	)
+		# print(time.time() - start_time)
 
 		#our code
 		optim = CombOptim(3, lambda comb: calculator.get_best_price(comb, region_to_check, pricing, architecture, type_major), updated_params)
-		return optim.run()
+		res += optim.run()
 
 		# First Step- match an instance for every component
 		# firstBranch = simplest_comb(updated_params, app_size)
