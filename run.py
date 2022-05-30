e280c4ae2fc229ead77d5b13ce997cac62a42edc
Goren <yosef.goren@intel.com>
2022-05-30 23:50:28 +0300

    added hyperparam experiments and hyperparam plot

--- run.py
+++ run.py
@@ -1,4 +1,5 @@
 from audioop import mul
+import re
 from Experiment import *
 from Distributions import NormDistInt
 
@@ -51,7 +52,6 @@ def one_big_sample_experiment()->Experiment:
         unique_sample_inputs = True
     )
 
-
 def create_time_price_experiment(N: int, C: int, T: int)->Experiment:
     name = f"time-price_N-{N}_C-{C}_T-{T}"
     return Experiment.create(
@@ -73,6 +73,29 @@ def create_time_price_experiment(N: int, C: int, T: int)->Experiment:
             "net": NormDistInt(1, 1, 1, 4)
         },
         unique_sample_inputs = True
+    )
+
+def create_candidate_size_experiement(N: int, C: int, T: int, Size: int)->Experiment:
+    name = f"candidate-size_N-{N}_C-{C}_T-{T}_Size-{Size}"
+    return Experiment.create(
+        experiment_name=name,
+        control_parameter_lists = {
+            "component_count":  [C]*N,
+            "time_per_region":  [float(T)]*N,
+            "significance":     [1]*N
+        },
+        algorithm_parameter_lists = {
+            "candidate_list_size":              [Size]*N,   
+            "exploitation_score_price_bias":    [0.5]*N,
+            "exploration_score_depth_bias":     [1.0]*N,
+            "exploitation_bias":                [0.8]*N
+        },
+        component_resource_distirubtions = {
+            "cpu": NormDistInt(3, 3, 1, 32),
+            "ram": NormDistInt(4, 4 ,1, 128),
+            "net": NormDistInt(1, 1, 1, 4)
+        },
+        region = "us-east-1"
     )
 
 def create_trail_experiment()->Experiment:
@@ -121,7 +144,21 @@ def plot_hyperparam_experiments(experiment_names: list, hyperparam_values: list)
 
 # variablses are: "INSERT_TIME", "NODES_COUNT", "ITERATION", "DEPTH_BEST", "BEST_PRICE"
 if __name__ == "__main__":
+    
+    sizes = [32*i for i in range(1, 11)]
+    N = 100
+    C = 18
+    T = 20
 
-    plot_hyperparam_experiments()
+    # for size in sizes:
+    #     e = create_candidate_size_experiement(N, C, T, size)
+    #     e.run(multiprocess=6, retry=2)
+
+    candidate_experiment_naems = [f"candidate-size_N-{N}_C-{18}_T-{T}_Size-{size}" for size in sizes]
+    plot_hyperparam_experiments(candidate_experiment_naems, sizes)
+    
+    bias_values = [i*2/10.0 for i in range(5)]
+    bias_experiment_names = [f"tation-bias_N-300_C-18_T-20_Bias-0.{i*2}" for i in range(5)]
+    plot_hyperparam_experiments(bias_experiment_names, bias_values)
     
  
